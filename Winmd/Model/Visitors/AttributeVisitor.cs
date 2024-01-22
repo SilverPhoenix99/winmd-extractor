67647a63aac443b7e3e89e2f41801a3b79c527aa
SilverPhoenix99 <silver.phoenix99@gmail.com>
2024-01-22 22:44:16 +0000

    Added special case for Guids.

--- Winmd/Model/Visitors/AttributeVisitor.cs
+++ Winmd/Model/Visitors/AttributeVisitor.cs
@@ -1,6 +1,7 @@
 ﻿namespace Winmd.Model.Visitors;
 
 using System.Collections.Immutable;
+using System.Runtime.InteropServices;
 using ClassExtensions;
 using Mono.Cecil;
 
@@ -8,28 +9,56 @@ class AttributeVisitor : IVisitor<CustomAttribute, AttributeModel>
 {
     public static readonly AttributeVisitor Instance = new();
 
+    private static readonly (string Name, string Namespace) GuidName = typeof(GuidAttribute).GetQualifiedName()!;
+
     private AttributeVisitor() {}
 
-    public AttributeModel Visit(CustomAttribute value)
+    public AttributeModel Visit(CustomAttribute attribute)
     {
-        var (name, ns) = value.AttributeType.GetQualifiedName();
+        var (name, @namespace) = attribute.AttributeType.GetQualifiedName();
         if (name.EndsWith("Attribute"))
         {
             name = name[..^9];
         }
 
+        if (name == "Guid" && @namespace == "Windows.Win32.Foundation.Metadata")
+        {
+            // Special case
+            return CreateGuid(attribute);
+        }
+
         var ctorArgs =
-            from arg in value.ConstructorArguments
+            from arg in attribute.ConstructorArguments
             select arg.Accept<AttributeArgumentModel>(AttributeArgumentVisitor.Instance);
 
         var namedArgs =
-            from arg in value.Fields.Concat(value.Properties)
+            from arg in attribute.Fields.Concat(attribute.Properties)
             select arg.Accept<AttributeArgumentModel>(AttributeArgumentVisitor.Instance);
 
         return new AttributeModel(name)
         {
-            Namespace = ns,
+            Namespace = @namespace,
             Arguments = ctorArgs.Concat(namedArgs).ToImmutableList()
         };
     }
+
+    private static AttributeModel CreateGuid(ICustomAttribute attribute)
+    {
+        var args = attribute.ConstructorArguments
+            .Select(a => a.Value)
+            .ToArray();
+
+        var guid = (Guid) Activator.CreateInstance(typeof(Guid), args)!;
+
+        return new AttributeModel(GuidName.Name)
+        {
+            Namespace = GuidName.Namespace,
+            Arguments = ImmutableList.Create(new AttributeArgumentModel(
+                StringType,
+                guid.ToString()
+            ))
+        };
+    }
+
+    private static readonly TypeModel StringType = new("String");
 }
