5c79dd715f74ae218614bf4904e822468ad77618
SilverPhoenix99 <silver.phoenix99@gmail.com>
2024-01-22 21:02:54 +0000

    Added a couple of model types.

--- Winmd/Model/ModelType.cs
+++ Winmd/Model/ModelType.cs
@@ -5,7 +5,9 @@ enum ModelType
     Apis,      // class Apis - the list of available constants and functions
     // Attribute - not used
     Callback,
+    Constant,  // defines or consts
     Enum,
+    Function,  // C functions
     Interface, // COM interfaces?
     Struct,    // struct or union
     Typedef,
