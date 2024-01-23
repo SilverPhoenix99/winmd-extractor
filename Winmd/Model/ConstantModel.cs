210e16eac37f5525a08b3c8c768f3f72cc89eee1
SilverPhoenix99 <silver.phoenix99@gmail.com>
2024-01-23 10:04:44 +0000

    Implemented edge cases for ConstantVisitor.

--- Winmd/Model/ConstantModel.cs
+++ Winmd/Model/ConstantModel.cs
@@ -1,9 +1,9 @@
 ﻿namespace Winmd.Model;
 
-class ConstantModel(string name) : BaseObjectModel(name)
+class ConstantModel(string name, TypeModel constantType) : BaseObjectModel(name)
 {
     public override ModelType Type => ModelType.Constant;
-    public TypeModel ConstantType { get; set; } = null!;
+    public TypeModel ConstantType => constantType;
     public object Value { get; set; } = null!;
     public TypeModel ValueType { get; set; } = null!;
 }
--- Winmd/Model/Visitors/ConstantVisitor.cs
+++ Winmd/Model/Visitors/ConstantVisitor.cs
@@ -10,27 +10,105 @@ class ConstantVisitor : IVisitor<FieldDefinition, ConstantModel>
 
     private ConstantVisitor() {}
 
-    public ConstantModel Visit(FieldDefinition field)
-    {
-        var constantName = field.Constant.GetType().GetQualifiedName();
+    public ConstantModel Visit(FieldDefinition field) => new Context(field).Visit();
 
-        var fieldType = field.FieldType.Accept(TypeVisitor.Instance);
-        if (fieldType.Namespace == field.DeclaringType.Namespace)
+    private class Context(FieldDefinition field)
+    {
+        public ConstantModel Visit() => VisitGuid() ?? VisitConstant() ?? VisitDefault();
+
+        private ConstantModel? VisitGuid()
         {
-            // Ignore namespace when it's enclosed in the same namespace as Apis
-            fieldType.Namespace = null;
+            if (field.FieldType.Name != TypeModel.GuidType.Name || field.HasConstant)
+            {
+                // If it has a constant, it'll be dealt with in VisitDefault
+                return null;
+            }
+
+            var guidAttribute = attributes.Value.FirstOrDefault(a => a.Name == TypeModel.GuidType.Name);
+            if (guidAttribute is null)
+            {
+                return null;
+            }
+
+            return new ConstantModel(field.Name, TypeModel.GuidType)
+            {
+                Attributes = attributes.Value
+                    .Where(a => a != guidAttribute)
+                    .ToImmutableList(),
+                Value = guidAttribute.Arguments[0].Value!,
+                ValueType = TypeModel.StringType
+            };
         }
 
-        var model = new ConstantModel(field.Name)
+        private ConstantModel? VisitConstant()
         {
-            Attributes = field.CustomAttributes
-                .Select(a => a.Accept(AttributeVisitor.Instance))
-                .ToImmutableList(),
-            ConstantType = fieldType,
-            Value = field.Constant,
-            ValueType = new TypeModel(constantName.Name) { Namespace = constantName.Namespace }
-        };
+            if (field.HasConstant)
+            {
+                // If it has a constant, it'll be dealt with in VisitDefault
+                return null;
+            }
 
-        return model;
+            var constAttribute = attributes.Value
+                .FirstOrDefault(a =>
+                    a is { Name: "Constant", Namespace: "Windows.Win32.Foundation.Metadata" }
+                );
+
+            if (constAttribute is null)
+            {
+                return null;
+            }
+
+            return new ConstantModel(field.Name, FieldType)
+            {
+                Attributes = attributes.Value
+                    .Where(a => a != constAttribute)
+                    .ToImmutableList(),
+                Value = constAttribute.Arguments[0].Value!,
+                ValueType = TypeModel.StringType
+            };
+        }
+
+        private ConstantModel VisitDefault()
+        {
+            var model = new ConstantModel(field.Name, FieldType)
+            {
+                Attributes = attributes.Value
+            };
+
+            if (!field.HasConstant)
+            {
+                return model;
+            }
+
+            var value = model.Value = field.Constant!;
+            var constantName = value.GetType().GetQualifiedName();
+            model.ValueType = new TypeModel(constantName.Name, constantName.Namespace);
+
+            return model;
+        }
+
+        private readonly Lazy<ImmutableList<AttributeModel>> attributes = new(() =>
+            field.CustomAttributes
+                .Select(a => a.Accept(AttributeVisitor.Instance))
+                .ToImmutableList()
+        );
+
+        private TypeModel FieldType
+        {
+            get
+            {
+                var fieldType = field.FieldType.Accept(TypeVisitor.Instance);
+                if (fieldType.Namespace == field.DeclaringType.Namespace)
+                {
+                    // Ignore namespace when it's enclosed in the same namespace as Apis
+                    return new TypeModel(fieldType.Name)
+                    {
+                        Modifiers = fieldType.Modifiers
+                    };
+                }
+
+                return fieldType;
+            }
+        }
     }
 }
