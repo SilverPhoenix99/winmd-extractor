a9d02984bc847e60ce22b88ac75aa86fd4c76521
SilverPhoenix99 <silver.phoenix99@gmail.com>
2024-01-23 16:03:52 +0000

    Swapped hierarchy of FunctionModel <-> CallbackModel.

---
+++ Winmd/Model/FunctionModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Winmd.Model;
+
+using System.Collections.Immutable;
+
+class FunctionModel(string name) : BaseObjectModel(name)
+{
+    public override ModelType Type => ModelType.Function;
+    public TypeModel ReturnType { get; set; } = null!;
+    public IImmutableList<CallbackArgumentModel> Arguments { get; set; } = ImmutableList<CallbackArgumentModel>.Empty;
+}
+
+class CallbackModel(string name) : FunctionModel(name)
+{
+    public override ModelType Type => ModelType.Callback;
+}
