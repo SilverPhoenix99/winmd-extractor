857961a5061a70e86676694af2cf58265ff85340
SilverPhoenix99 <silver.phoenix99@gmail.com>
2024-01-25 18:36:24 +0000

    Immutable TypeModel.

--- Winmd/Model/TypeModel.cs
+++ Winmd/Model/TypeModel.cs
@@ -4,20 +4,16 @@ using System.Collections.Immutable;
 using System.Runtime.InteropServices;
 using ClassExtensions;
 
-class TypeModel(string name, string? @namespace = null)
+record TypeModel(string Name, string? Namespace = null, IImmutableList<TypeModifier>? Modifiers = null)
 {
-    public string Name => name;
-    public string? Namespace => @namespace;
-    public IImmutableList<TypeModifier>? Modifiers { get; set; }
-
     #region Constants
 
-    public static readonly TypeModel GuidType = new("Guid");
+    public static readonly TypeModel GuidType = new(nameof(Guid));
 
     public static readonly (string Name, string Namespace) LayoutKindName = typeof(LayoutKind).GetQualifiedName()!;
     public static readonly TypeModel LayoutKindType = new(LayoutKindName.Name, LayoutKindName.Namespace);
 
-    public static readonly TypeModel StringType = new("String");
+    public static readonly TypeModel StringType = new(nameof(String));
 
     #endregion
 }
